1cdf73d4f14044569ae90b2f924748031705deca
RoohiniR2194 <rohinir2194@gmail.com>
2018-12-09 00:46:02 +0530

    Added changes for new component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'customer-app'`, () => {
+  it(`should have as title 'TestApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('customer-app');
+    expect(app.title).toEqual('TestApp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to customer-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TestApp!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHome } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, AppHome
   ],
   imports: [
     BrowserModule,
