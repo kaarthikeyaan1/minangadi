5eb47ec3c6e7de199dde9411658cec343513225e
Rohini R <45301526+RohiniR2194@users.noreply.github.com>
2018-12-25 15:50:43 +0530

    1. Updated Header icon and handled click event to navigate to home page, 2. Created new components for Category, Favourites, 3. Implemented Search component and back button updates, 4. Responsiveness and toolbar menu correction in home page (#7)

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../data.service";
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.component.html',
   styleUrls: ['./favourites.component.css']
 })
-export class FavouritesComponent implements OnInit {
+export class FavouritesComponent implements OnInit{
 
-  constructor() { }
-
+  constructor(private data: DataService) { }
+  
+  //TODO change this to interface
+ breakpoint = {
+  breakpointMain:2,
+  breakpointImage:1,
+  breakpointContent:1
+}
+ 
   ngOnInit() {
-  }
+    //this.breakpointMain = this.calcBreakPoint();
 
+    this.data.changeMessage("ShowHeader");
+    this.calcBreakPoint();
+  }
+  onResize(event) {
+    this.calcBreakPoint();
+  }
+  calcBreakPoint(){
+    if(window.innerWidth <= 1199){ //xs sm md
+      this.breakpoint.breakpointMain = 2;
+      this.breakpoint.breakpointImage = 1;
+      this.breakpoint.breakpointContent = 1;
+    }
+    else //lg
+    {
+      this.breakpoint.breakpointMain = 6;
+      this.breakpoint.breakpointImage = 1;
+      this.breakpoint.breakpointContent = 5;
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../data.service";
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  bshowHeader:boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private data: DataService) { 
   }
+  ngOnInit() {
+    this.bshowHeader = true;
 
+    this.data.currentMessage.subscribe(message => {
+      if(message == "ShowHeader")
+        this.showHeader();
+      else if (message == "HideHeader")
+        this.hideHeader();
+    })
+  }
+  hideHeader(){
+    this.bshowHeader = false;
+  }
+  showHeader(){
+    this.bshowHeader = true;
+  }
 }
